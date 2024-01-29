75b0c95b8bdc95ec7a531cc7061b61d3e997d264
lajain Alghamdi <louginn67@gmail.com>
2024-01-29 11:28:52 +0300

    simple print

---
+++ src/cpit251/CPIT251.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpit251;
+
+/**
+ *
+ * @author 2207786
+ */
+public class CPIT251 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
